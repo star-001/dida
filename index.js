5bbf4ed66b3b1880a1e114a4f02d0b36122ff490
star-001 <fanzhenxin@outlook.com>
2020-04-27 17:21:17 +0800

    Add icon

--- index.js
+++ index.js
@@ -1,6 +1,53 @@
-const { app, BrowserWindow, Menu } = require('electron')
+const { app, BrowserWindow, Menu, Tray } = require('electron')
+let tray = null
 Menu.setApplicationMenu(null);
+
+function createWindow(){
+
+    win = new BrowserWindow();
+    win.loadURL('https://dida365.com/sigin');
+
+    win.on('closed',(event)=>{
+        win = null;
+    });
+
+    win.on('close',(event)=>{
+        win.hide();
+        win.setSkipTaskbar(true);
+        event.preventDefault();
+    })
+
+}
 app.on('ready', ()=>{
-	let win = new BrowserWindow()
-	win.loadURL('https://dida365.com/signin')
+	tray = new Tray('/home/fan/Code/dida/dida.jpg')
+	let win = new BrowserWindow();
+	var trayMenuTemplate = [
+        {
+            label: '设置',
+            click: function () {} //打开相应页面
+        },
+        {
+            label: '帮助',
+            click: function () {}
+        },
+        {
+            label: '关于',
+            click: function () {}
+        },
+        {
+            label: '退出',
+            click: function () {
+                //ipc.send('close-main-window');
+                 app.quit();
+            }
+        }
+    ];
+	const contextMenu=Menu.buildFromTemplate(trayMenuTemplate);
+    tray.setContextMenu(contextMenu);
+    tray.on('click',()=>{
+        win.isVisible()?win.hide():win.show()
+        win.isVisible()?win.setSkipTaskbar(false):win.setSkipTaskbar(true);
+    })
+
+	win.loadURL('https://dida365.com/signin');
 })
